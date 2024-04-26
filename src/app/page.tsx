ee3ef30fc878168ed9df3f904006a79e9451947d
Dung <dinhtuandungnguyen@gmail.com>
2024-04-26 17:44:27 +0700

    26/4

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import Image from 'next/image'
+
+
+
+export default function Home() {
+  return (
+    <main>
+      <div className='q'><h2></h2></div>
+      <div className='z'><h2>PHIM ĐANG CHIẾU</h2></div>
+    </main >
+  )
+}
