085943a9c6d985916fd6f54d15f59a39863736d7
Dung <dinhtuandungnguyen@gmail.com>
2024-05-24 21:55:01 +0700

    thong bao realtime

--- src/app/movies/[movieid]/page.tsx
+++ src/app/movies/[movieid]/page.tsx
@@ -1,27 +1,27 @@
 'use client'
-import React from 'react'
-import { BsShare } from 'react-icons/bs'
-import { BsFillStarFill } from 'react-icons/bs';
-import './MoviePage.css'
+import React, { useState, useEffect } from 'react';
+import { BsShare, BsFillStarFill } from 'react-icons/bs';
+import './MoviePage.css';
 import MovieCarousel from '@/components/MovieCarousel/MovieCarousel';
-
-
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
 import CelebCard from '@/components/CelebCard/CelebCard';
-import { usePathname, useParams } from 'next/navigation'
+import { usePathname, useParams } from 'next/navigation';
 import Link from 'next/link';
 import MovieCarouselExceptCurrentFilm from '@/components/MovieCarousel/MovieCarouselExceptCurrentFilm';
 
-
 const MoviePage = () => {
-    const pathname = usePathname()
-    const { movieid } = useParams()
-    console.log(movieid)
-    const [movie, setMovie] = React.useState<any>(null)
-    console.log(movieid)
+    const pathname = usePathname();
+    const { movieid } = useParams();
+    const [movie, setMovie] = useState<any>(null);
+    const [rating, setRating] = useState(0);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [userId, setUserId] = useState('');
+    const [avgRating, setAvgRating] = useState(0); // Thêm state để lưu trữ avgRating
+    const [numberOfRatings, setNumberOfRatings] = useState(0);
+
 
     const getMovie = async () => {
         fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/movie/movies/${movieid}`, {
@@ -31,77 +31,166 @@ const MoviePage = () => {
             },
             credentials: 'include'
         })
-            .then((res) => res.json())
-            .then((data) => {
-                if (data.ok) {
-                    console.log(data)
-                    setMovie(data.data)
-                }
-            })
-            .catch((err) => {
-                console.log(err)
-            })
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.ok) {
+                setMovie(data.data);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    };
+    const getAvgRating = async () => {
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/movie/avgRating/${movieid}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include'
+            });
+            const data = await response.json();
+            if (data.avgRating) {
+                setAvgRating(data.avgRating);
+                setNumberOfRatings(data.numberOfRatings); // Thêm setNumberOfRatings để lưu trữ số lượt đánh giá
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    
+    const checkLoginStatus = async () => {
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/auth/checklogin`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include'
+            });
+            const data = await response.json();
+            if (data.ok) {
+                setUserId(data.userId);
+                setIsLoggedIn(true);
+            } else {
+                setIsLoggedIn(false);
+            }
+        } catch (error) {
+            console.error('Error checking login status:', error);
+            setIsLoggedIn(false);
+        }
+    };
+    
 
+    const handleRating = () => {
+        // Kiểm tra xem người dùng đã đăng nhập chưa
+        if (!isLoggedIn) {
+            // Nếu chưa đăng nhập, chuyển hướng đến trang đăng nhập
+            window.location.href = '/login';
+            return;
+        }
+    
+        // Kiểm tra xem người dùng đã đánh giá chưa bằng cách gọi API
+        fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/movie/checkUserRating/${movieid}/${userId}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include'
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.hasRated) {
+                // Nếu đã đánh giá, hiển thị thông báo
+                alert('Bạn đã đánh giá cho bộ phim này.');
+            } else {
+                // Nếu chưa đánh giá, thực hiện POST request để gửi đánh giá mới
+                fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/movie/rating`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    credentials: 'include',
+                    body: JSON.stringify({ movieId: movieid, userId: userId, rating: rating })
+                })
+                .then((res) => res.json())
+                .then((data) => {
+                    if (data.success) {
+                        // Nếu thành công, hiển thị thông báo và cập nhật lại thông tin phim
+                        alert('Đánh giá của bạn đã được ghi nhận.');
+                        getMovie(); // Cập nhật lại thông tin phim sau khi đánh giá thành công
+                    } else {
+                        // Nếu không thành công, hiển thị thông báo lỗi
+                        alert('Đánh giá không thành công, vui lòng thử lại.');
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                    alert('Có lỗi xảy ra, vui lòng thử lại.');
+                });
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            alert('Có lỗi xảy ra, vui lòng thử lại.');
+        });
+    };
+    
+    
 
+    useEffect(() => {
+        getMovie();
+        checkLoginStatus();
+    }, []);
 
-
-    }
-
-    React.useEffect(() => {
-        getMovie()
-    }, [])
     const formattedDate = movie ?
         `${new Date(movie.releasedate).getDate()}/${new Date(movie.releasedate).getMonth() + 1}/${new Date(movie.releasedate).getFullYear()}`
         : '';
 
-
+        getAvgRating();
     return (
         <>
             {movie &&
                 <div className='moviepage'>
-                    <div className='c1' style={{
-                        backgroundImage: `url(${movie.landscapeImgUrl})`
-                    }}>
+                    <div className='c1' style={{ backgroundImage: `url(${movie.landscapeImgUrl})` }}>
                         <div className='c11'>
                             <div className='left'>
-                                <div className='movie_poster'
-                                    style={{
-                                        backgroundImage: `url(${movie.portraitImgUrl})`
-                                    }}
-                                >
+                                <div className='movie_poster' style={{ backgroundImage: `url(${movie.portraitImgUrl})` }}>
                                 </div>
                                 <div className='movie_details'>
-                                    <p className='title'>
-                                        {movie.title}
-                                    </p>
+    <div className='rating-section'>
+    <p className='rating'>
+    <BsFillStarFill className='star' />&nbsp;&nbsp;{avgRating.toFixed(1)}/10    &nbsp;({numberOfRatings} đánh giá) 
+</p>
 
-                                    <span className='releasedate'>
-                                        Đạo diễn:  {movie.director}
-                                    </span>
-                                    <p className='duration_type_releasedat'>
-                                        <span className='duration'>
-                                            Thời lượng: {movie.duration} phút
-                                        </span>
-                                    </p>
-                                    <span className='type'>
-                                        Thể loại: {movie.genre.join(', ')}
-                                    </span>
-                                    <span className='releasedate'>
-                                        Khởi chiếu:  {formattedDate}
-                                    </span>
-                                    <span className='releasedate'>
-                                        Rated:  {movie.rated}
-                                    </span>
-                                    <Link
-                                        href={`${pathname}/buytickets`}
-                                        className='linkstylenone'
-                                    >
-                                        <button className='bookbtn'>Đặt vé</button>
-                                    </Link>
+        <div className="rating-input">
+            <label htmlFor='rating'>Đánh giá của bạn:</label>
+            <select 
+                id='rating' 
+                value={rating} 
+                onChange={(e) => setRating(parseInt(e.target.value))}
+            >
+{Array.from(Array(11).keys()).map(num => (
+                    <option key={num} value={num}>{num}</option>
+                ))}
+            </select>
+            <button onClick={handleRating}>Đánh giá</button>
+        </div>
+    </div>
+    <span className='releasedate'>Đạo diễn: {movie.director}</span>
+    <p className='duration_type_releasedat'>
+        <span className='duration'>Thời lượng: {movie.duration} phút</span>
+    </p>
+    <span className='type'>Thể loại: {movie.genre.join(', ')}</span>
+    <span className='releasedate'>Khởi chiếu: {formattedDate}</span>
+    <span className='releasedate'>Rated: {movie.rated}</span>
+    <Link href={`${pathname}/buytickets`} className='linkstylenone'>
+        <button className='bookbtn'>Đặt vé</button>
+    </Link>
+</div>
 
-                                </div>
                             </div>
-
                         </div>
                     </div>
                     <div className='c2'>
@@ -111,12 +200,9 @@ const MoviePage = () => {
                         <h1>Có thể bạn cũng thích</h1>
                     </div>
                     <div className='g'><MovieCarouselExceptCurrentFilm /></div>
-
-
                 </div>
             }
         </>
-    )
+    );
 }
-
 export default MoviePage
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,131 +1,285 @@
-'use client'
-import Link from 'next/link'
-import React from 'react'
-import './Navbar.css'
-import { BiUserCircle, BiSearch } from 'react-icons/bi'
-import { RiArrowDropDownFill } from 'react-icons/ri'
-import logo from '@/assets/logo.png'
-import Image from 'next/image'
+"use client";
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import './Navbar.css';
+import { BiUserCircle, BiBell } from 'react-icons/bi';
+import logo from '@/assets/logo.png';
+import Image from 'next/image';
+import io from 'socket.io-client';
+import { toast } from 'react-toastify';
 
+// Define the notification type
+interface Notification {
+    _id: string;
+    userId: string;
+    message: string;
+    isRead: boolean;
+    timestamp: Date;
+}
 
+const Navbar: React.FC = () => {
+    const [showLocationPopup, setShowLocationPopup] = useState<boolean>(false);
+    const [user, setUser] = useState<any>(null);
+    const [userID, setUserID] = useState<any>(null);
 
-const Navbar = () => {
-    const [showLocationPopup, setShowLocationPopup] = React.useState<boolean>(false)
-    const [user, setUser] = React.useState<any>(null)
-    const [loggedIn, setLoggedIn] = React.useState<boolean>(false)
+    const [loggedIn, setLoggedIn] = useState<boolean>(false);
+    const [notifications, setNotifications] = useState<Notification[]>([]);
+    const [showNotifications, setShowNotifications] = useState<boolean>(false);
+    const [unreadNotifications, setUnreadNotifications] = useState(0);
+    const [shouldReload, setShouldReload] = useState(false);
     const getuser = async () => {
+        try {
+            const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/auth/getuser`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include'
+            });
+            const response = await res.json();
 
-        fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/auth/getuser`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            credentials: 'include'
-        })
-            .then((res) => {
-                return res.json();
-            })
-            .then((response) => {
-                console.log(response)
-                setUser(response.data)
-            })
-            .catch((error) => {
-                console.log(error)
-            })
+            console.log(response.data);
+            setUser(response.data);
+            setUserID(response.data.userId);
+            console.log("11111");
+            console.log(user);
 
-    }
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+   
+    useEffect(() => {
+        const socket = io("http://localhost:8000"); // Connect to Socket.IO server
+        socket.on('connect', () => {
+            console.log('Connected to Socket.IO server');                    
+        });
+
+        socket.on('bookingConfirmed', (data: { userId: string, message: string }) => {
+            console.log('New notification received:', data.message); 
+            const newNotification: Notification = { 
+                _id: '', // Phải điền giá trị cho _id
+                userId: data.userId, 
+                message: data.message, 
+                isRead: false, // Có thể đặt giá trị mặc định cho isRead
+                timestamp: new Date() // Đặt thời gian cho timestamp
+            };
+          
+            // Kiểm tra xem thông báo có userId của người dùng hiện tại hay không
+            if (newNotification.userId === user.userId) {
+                setShouldReload(true); // Đánh dấu để reload component
+                // Cập nhật notifications với một mảng mới
+                setNotifications(prevNotifications => [newNotification, ...prevNotifications]);
+                if (!newNotification.isRead) {
+                    setUnreadNotifications(prevCount => prevCount + 1);
+                }
+                setShowNotifications(true);
+                fetchNotifications();
+            }
+        });
+        
+        
+        
+
+        // Fetch initial notifications
+        fetchNotifications();
+        const notificationIcon = document.querySelector('.notification-icon');
+        if (notificationIcon) {
+          notificationIcon.classList.add('has-unread');
+          // Cập nhật badge thông báo (nếu có)
+          const notificationBadge = notificationIcon.querySelector('.notification-badge');
+          if (notificationBadge) {
+            notificationBadge.textContent = unreadNotifications.toString();
+          }
+        }
+        // Cleanup on component unmount
+        return () => {
+            socket.disconnect();
+        };
+    }, [user]);
+
+    useEffect(() => {
+        if (shouldReload) {
+          // Fetch lại notifications và các thông tin cần thiết khác
+          fetchNotifications();
+          getuser(); // Ví dụ: gọi lại API để lấy thông tin người dùng
+          checkLogin(); // Ví dụ: kiểm tra lại trạng thái đăng nhập
+    
+          // Reset shouldReload về false sau khi đã xử lý
+          setShouldReload(false);
+        }
+      }, [shouldReload]); // Chỉ chạy lại khi shouldReload thay đổi
+
+   
 
     const handleLogout = async () => {
-        fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/auth/logout`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            credentials: 'include'
-        })
-            .then((res) => {
-                return res.json();
-            })
-            .then((response) => {
-                console.log(response)
-                if (response.ok) {
-                    if (typeof window !== 'undefined') {
-                        window.location.href = "/auth/signin"
-                    }
-                }
-
-            })
-            .catch((error) => {
-                console.log(error)
+        try {
+            const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/auth/logout`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include'
+            });
+            const response = await res.json();
+            if (response.ok) {
                 if (typeof window !== 'undefined') {
-                    window.location.href = "/auth/signin"
+                    window.location.href = "/auth/signin";
                 }
-            })
-    }
+            }
+        } catch (error) {
+            console.log(error);
+            if (typeof window !== 'undefined') {
+                window.location.href = "/auth/signin";
+            }
+        }
+    };
 
     const checkLogin = async () => {
-        fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/auth/checklogin`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            credentials: 'include'
-        })
-            .then((res) => {
-                return res.json();
-            })
-            .then((response) => {
-                console.log(response)
-                if (response.ok) {
-                    setLoggedIn(true)
-                }
-                else {
-                    setLoggedIn(false)
-                }
-            })
-            .catch((error) => {
-                console.log(error)
-                setLoggedIn(false)
-            })
-    }
+        try {
+            const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/auth/checklogin`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include'
+            });
+            const response = await res.json();
+            setLoggedIn(response.ok);
+        } catch (error) {
+            console.log(error);
+            setLoggedIn(false);
+        }
+    };
 
-    React.useEffect(() => {
-        checkLogin()
-        getuser()
-    }, [])
+    const fetchNotifications = async () => {
+        try {
+            const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/notifications/getnoti`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include'
+            });
+            const response = await res.json();
+            const fetchedNotifications = response.data;
+            setNotifications(fetchedNotifications.slice(0, 5)); // Get the latest 10 notifications
+            const unreadCount = fetchedNotifications.filter((noti: Notification) => !noti.isRead).length;
+            setUnreadNotifications(unreadCount);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        checkLogin();
+        getuser();
+        fetchNotifications();
+    }, [showNotifications]);
+
+    const handleNotificationClick = () => {
+        setShowNotifications(!showNotifications);
+        if (unreadNotifications > 0) {
+            setUnreadNotifications(0);
+        }
+    };
+
+    const handleNotificationItemClick = async (notificationId: string) => {
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/notifications/update/${notificationId}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include',
+                body: JSON.stringify({ isRead: true })
+            });
+
+            if (!response.ok) {
+                throw new Error(`Failed to update notification: ${response.statusText}`);
+            }
+
+            setNotifications(prevNotifications => prevNotifications.map(noti => 
+                noti._id === notificationId ? { ...noti, isRead: true } : noti
+            ));
+        } catch (error) {
+            console.error("Error updating notification:", error);
+        }
+    };
+
+    useEffect(() => {
+        // Chờ cho đến khi component được mount và các phần tử đã được tạo ra
+        const updateNotificationIcon = () => {
+          const notificationIcon = document.querySelector('.notification-icon');
+          if (notificationIcon) {
+            if (unreadNotifications > 0) {
+              notificationIcon.classList.add('has-unread');
+            } else {
+              notificationIcon.classList.remove('has-unread');
+            }
+      
+            // Cập nhật badge thông báo (nếu có)
+            const notificationBadge = notificationIcon.querySelector('.notification-badge');
+            if (notificationBadge) {
+              notificationBadge.textContent = unreadNotifications.toString();
+            }
+          } else {
+            // Nếu phần tử chưa tồn tại, thử lại sau một khoảng thời gian ngắn
+            setTimeout(updateNotificationIcon, 100); // Thử lại sau 100ms
+          }
+        };
+      
+        updateNotificationIcon(); // Gọi hàm cập nhật lần đầu
+      }, [unreadNotifications]); // Gọi lại khi unreadNotifications thay đổi
+
     return (
         <nav>
             <div className='left'>
-                <Image src={logo} alt="logo" width={100} height={100}
-                    onClick={() => window.location.href = "/"}
-                />
-
+                <Image src={logo} alt="logo" width={100} height={100} onClick={() => window.location.href = "/"} />
             </div>
 
             <div className='right'>
                 {loggedIn && user ? (
-
                     <div className="user-info">
                         <a className="q" style={{ cursor: 'pointer' }} href="/promotion">Tin tức, ưu đãi</a>
-
-                        <div className='fd'><BiUserCircle className='theme_icon1' /></div>
+                        
+                        <BiUserCircle className='theme_icon1' />
                         <a href='/profile'>Xin chào, {user.name}! </a>
-                        <button className='theme_btn1 linkstylenone' style={{ cursor: 'pointer' }} onClick={handleLogout}>Đăng xuất</button>
+                        <div className={`notification-icon ${unreadNotifications > 0 ? 'has-unread' : ''}`} onClick={handleNotificationClick}>
+                            <BiBell className='theme_icon1' />
+                            {unreadNotifications > 0 && (
+                                <span className="notification-badge">{unreadNotifications}</span>
+                            )}
+                        </div>
+                        {showNotifications && (
+    <div className="notifications-dropdown">
+        {notifications.length > 0 ? (
+            notifications.map((notification, index) => (
+                <div 
+                    key={index} 
+                    className={`notification-item ${notification.isRead ? 'read' : 'unread'}`}
+                    onClick={() => handleNotificationItemClick(notification._id)}
+                >
+                    {notification.message} - {new Date(notification.timestamp).toLocaleString()}
+                </div>
+            ))
+        ) : (
+            <div className="notification-item">No notifications</div>
+        )}
+    </div>
+)}
+                        <button className='theme_btn1 linkstylenone' style={{ cursor: 'pointer' }} onClick={handleLogout}> Đăng xuất</button>
                     </div>
                 ) : (
                     <div>
                         <a className="q" style={{ cursor: 'pointer' }} href="/promotion">Tin tức, ưu đãi</a>
-                        <Link href="/auth/signin" className='theme_btn1 linkstylenone'>
-
-                            Đăng nhập
-                        </Link>
+                        <Link href="/auth/signin" className='theme_btn1 linkstylenone'>Đăng nhập</Link>
                     </div>
                 )}
-
             </div>
-           
         </nav>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
